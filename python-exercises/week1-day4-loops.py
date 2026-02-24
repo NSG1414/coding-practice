b774d7ee3011fc97aae1960bed66f78b3b2bb110
Namazzi Sanyu Gloria <namazzigloria41@gmail.com>
2026-02-24 12:27:46 +0300

    Week 1 Whil loops practice

---
+++ python-exercises/week1-day4-loops.py
@@ -0,0 +1,12 @@
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print("I like", fruit)
+
+#i like apples
+#i like banana
+#i like cherry
+
+count = 5
+while count > 0:
+    print("Count is", count)
+    count -= 1
