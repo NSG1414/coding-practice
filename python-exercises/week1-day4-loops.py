1c6abb17a583df71b388a5bb9a90c537090a71f3
Namazzi Sanyu Gloria <namazzigloria41@gmail.com>
2026-02-24 12:33:19 +0300

    Made some changes to my loop

--- python-exercises/week1-day4-loops.py
+++ python-exercises/week1-day4-loops.py
@@ -11,3 +11,8 @@ count = 5
 while count > 0:
     print("Count is", count)
     count -= 1
+
+#list of my favourite food
+foods = [rice, goat, chicken, gnuts]
+for food in foods:
+    print("I love <" food ">")
