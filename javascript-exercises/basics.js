f20df4507d5cfd4ff1437b5bc4345ce93a8540f1
Namazzi Sanyu Gloria <namazzigloria41@gmail.com>
2026-03-03 20:14:49 +0300

    Trying to learn Javascript language alongside Django, installed vscode

---
+++ javascript-exercises/basics.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JavaScript Exercises</title>
+</head>
+<body>
+    <h1>JS Basics Practice</h1>
+    <p id="demo"></p>
+
+    <script src="basics.js"></script>
+</body>
+</html>
---
+++ javascript-exercises/test.py
@@ -0,0 +1,8 @@
+friends = {
+    "John": 21,
+    "Mary": 20,
+    "Peter": 22
+}
+
+for name, age in friends.items():
+    print(name, age)
