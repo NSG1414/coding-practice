d65b694a3c1d496075e64c373e3983492db6aec4
Namazzi Sanyu Gloria <namazzigloria41@gmail.com>
2026-03-03 20:17:14 +0300

    studied some oop concepts, especially inheritance to help me understand django more, now i am making a mini simple bank account

--- python-exercises/bank.py
+++ python-exercises/bank.py
@@ -3,6 +3,14 @@ class BankAccount:
         self.owner = owner
         self.balance = balance
         
-    def deposit(self, amoount):
+    def deposit(self, amount):
         self.balance += amount
         return f"{amount} deposited. New balance is {self.balance}"
+        
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return "Not enough funds"
+        self.balance -= amount
+        return f"{amount} withdrawn. New balance is {self.balance}"
+
+        
