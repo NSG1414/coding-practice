07db109b50a65b59bed15e05925b8e5f8e473917
Namazzi Sanyu Gloria <namazzigloria41@gmail.com>
2026-03-04 23:10:37 +0300

    Successfully made my code work by debugging and getting the desired output as i needed

--- python-exercises/bank.py
+++ python-exercises/bank.py
@@ -12,9 +12,23 @@ class BankAccount:
             return "Not enough funds"
         self.balance -= amount
         return f"{amount} withdrawn. New balance is {self.balance}"
-
+        
+    def check_balance(self):
+        return f"{self.owner}'s balance is {self.balance}"
+        
+    def transfer(self, amount, other_account):
+        if amount > self.balance:
+            return "Insufficient Funds"
+        self.balance -= amount
+        other_account.balance += amount
+        return f"{amount} transferred from {self.owner} to {other_account.owner}"
+        
 account1 = BankAccount("Glory", 1000) 
+account2 = BankAccount("Elijah", 500)
+
+print(account1.transfer(300, account2))
 print(account1.check_balance())
+print(account2.check_balance())
 print(account1.deposit(500))
 print(account1.withdraw(200))
 print(account1.withdraw(2000))       
