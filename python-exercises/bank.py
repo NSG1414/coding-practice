102ac03c0a7bc88029b62a253de6ac2d36871587
Namazzi Sanyu Gloria <namazzigloria41@gmail.com>
2026-03-04 22:18:00 +0300

    Created and printed objects for my BankAccount

--- python-exercises/bank.py
+++ python-exercises/bank.py
@@ -13,4 +13,8 @@ class BankAccount:
         self.balance -= amount
         return f"{amount} withdrawn. New balance is {self.balance}"
 
-        
+account1 = BankAccount("Glory", 1000) 
+print(account1.check_balance())
+print(account1.deposit(500))
+print(account1.withdraw(200))
+print(account1.withdraw(2000))       
